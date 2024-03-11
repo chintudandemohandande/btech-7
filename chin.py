47823c4d2f56d843971fb38ff07518b9c59b53ce
chintudandemohandande <162981811+chintudandemohandande@users.noreply.github.com>
2024-03-11 16:00:34 +0530

    Add files via upload

---
+++ chin.py
@@ -0,0 +1,9 @@
+'''
+1.)comminting is used for making documentation in a python file
+2.)when the line is connected ,that line will not be considered as a part of the code
+3.)when the program is executed ,commented line will not be in the part of executed
+----->NOTE:
+single line commenting can be done
+'''
+import this
+print (this)
